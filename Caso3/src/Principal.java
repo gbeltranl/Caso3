2f5a5128cfb4c3073c95205f715e1235c1d7d815
gbeltranl <g.beltranl@uniandes.edu.co>
2022-05-09 21:24:18 -0500

    Key pairs

--- Caso3/src/Principal.java
+++ Caso3/src/Principal.java
@@ -1,4 +1,29 @@
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Principal {
 
+	public static void main(String[] args) {
+		
+		KeyPairGenerator keyGen = null;
+		try {
+			keyGen = KeyPairGenerator.getInstance("RSA");
+			keyGen.initialize(1024);
+		    KeyPair keypair = keyGen.genKeyPair();
+		    PrivateKey privateKey = keypair.getPrivate();
+		    PublicKey publicKey = keypair.getPublic();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	    
+	    int a = 0;
+	}
 }
--- Caso3/src/Servidor.java
+++ Caso3/src/Servidor.java
@@ -1,11 +1,15 @@
 /**
  * 
  */
-
+import java.security.*;
+import javax.crypto.*;
 /**
  * @author Gabriel
  *
  */
 public class Servidor {
+	private String nombreCliente;
+	private long identificadorPaquete;
+	private String estadoPaquete;
 
 }
