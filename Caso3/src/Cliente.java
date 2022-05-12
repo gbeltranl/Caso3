dec024307843bc3641f3fa8dcf7f56d6a4b770df
gbeltranl <g.beltranl@uniandes.edu.co>
2022-05-11 22:52:19 -0500

    Tipos de clientes

--- Caso3/src/Cliente.java
+++ Caso3/src/Cliente.java
@@ -9,32 +9,70 @@ import java.util.Random;
 import java.util.Scanner; 
 
 
-public class Cliente {
+public class Cliente extends Thread{
 
 	private static Encriptador ec;
 	private static File archivoLlave;
 	private static PublicKey llaveServidor;
+	private String nombreCliente;
+	private int numPaqueteCliente;
 
 
 
+	public Cliente(String nombre, int idPaquete) {
+		nombreCliente = nombre;
+		numPaqueteCliente = idPaquete;
+	}
+
+	private void cambiarPaquete(int idPaquete) {
+		numPaqueteCliente= idPaquete;
+	}
 	public static void main(String[] args) {
 		ec = new Encriptador();
 		archivoLlave = new File("./docs/llavePublicaServidor.txt");
-		Cliente cliente1 = new Cliente();
+		System.out.println("Tipo de cliente: \n1)Iterativo \n2)Concurrente");
+		Scanner scan = new Scanner(System.in);  
+		int opcion = scan.nextInt(); 
+		switch (opcion) {
+		case 1: 
+			Cliente cliente = new Cliente("Nombre", 1);
+			for (int i = 0; i < 32; i++) {
+				//TODO acceder a la tabla
+				cliente.protocolo(cliente.nombreCliente, cliente.numPaqueteCliente);
+			}
+			break;
+		case 2:
+			System.out.println("Ingrese el numero de clientes");
+			int numClientes = scan.nextInt(); 
+			Cliente[] clientes = new Cliente[numClientes];
+			for (int i = 0; i < clientes.length; i++) {
+				//TODO acceso a la tabla
+				clientes[i] = new Cliente("nombre", 1);
+				clientes[i].run();
+			}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + opcion);
+		}
 
-		cliente1.protocolo();
+
+
 
 	}
+	@Override
+	public void run() {
+		protocolo(nombreCliente, numPaqueteCliente);
+	}
+	
 	private String generateInt(int digits) {
-	    StringBuilder str = new StringBuilder();
-	    Random random = new Random();
-	    for(int i = 0; i < digits; i++) {
-	        str.append(random.nextInt(10));
-	    }
-	    return str.toString();
+		StringBuilder str = new StringBuilder();
+		Random random = new Random();
+		for(int i = 0; i < digits; i++) {
+			str.append(random.nextInt(10));
+		}
+		return str.toString();
 	}
 
-	public void protocolo() {
+	public void protocolo(String nombre, int idPaquete) {
 
 		System.out.println("Iniciando Cliente...");
 
@@ -56,71 +94,63 @@ public class Cliente {
 			//Inicio Sesion
 			Scanner scan = new Scanner(System.in);  
 
-			System.out.println("Desea Iniciar sesi�n? : a) S�  b) No");
-
-			String mensaje = scan.nextLine(); 
-
-			if(mensaje.equals("a") ) {
-
-				flujoSalida.writeUTF("Iniciar sesion");
-
-				System.out.println("Iniciando Sesi�n...");
 
 
 
-				// Llegada de Mensaje ACK
-				String mensajeLlegada = flujoEntrada.readUTF();
+			flujoSalida.writeUTF("Iniciar sesion");
 
-				System.out.println(mensajeLlegada);
-
-
-				// El cliente env�a al servidor un reto (un n�mero aleatorio de 24 d�gitos)
-				String reto = generateInt(24);
-				flujoSalida.writeUTF(reto);
-
-				// LLega el reto cifrado
-				String retoCifrado = flujoEntrada.readUTF();
-				String decifrado = ec.decryptMessageAsymmetric(retoCifrado, llaveServidor);
-				boolean retoCorrecto = decifrado.equals(reto);
-
-				//  Al recibir la respuesta, el cliente valida que el reto cifrado tenga el valor esperado; si la validaci�n pasa entonces
-				//      el cliente contin�a con el protocolo; si la validaci�n no pasa entonces el cliente termina la comunicaci�n. 
-
-				if (retoCorrecto) {
-					System.out.println("Reto Correcto");
-
-					//TODO El cliente genera una llave sim�trica (LS),
-					//     la cifra con la llave p�blica del servidor (KS+) y la env�a al servidor. 
-					Key llaveSimetrica = ec.generateSymmetricKey();
-					String llaveEncriptada = ec.encryptKeyAsymmetric(llaveSimetrica, llaveServidor);
-					flujoSalida.writeUTF(llaveEncriptada);
-
-					// LLega la confirmaci�n ACK
-					String confirmacion = flujoEntrada.readUTF();
-
-					System.out.println(confirmacion);
+			System.out.println("Iniciando Sesi�n...");
 
 
-					// TODO El cliente env�a su nombre y espera un mensaje de confirmaci�n (�ACK�)
+
+			// Llegada de Mensaje ACK
+			String mensajeLlegada = flujoEntrada.readUTF();
+
+			System.out.println(mensajeLlegada);
 
 
-					System.out.println("Escriba su nombre: ");
+			// El cliente env�a al servidor un reto (un n�mero aleatorio de 24 d�gitos)
+			String reto = generateInt(24);
+			flujoSalida.writeUTF(reto);
 
-					String nombre = scan.nextLine();
+			// LLega el reto cifrado
+			String retoCifrado = flujoEntrada.readUTF();
+			String decifrado = ec.decryptMessageAsymmetric(retoCifrado, llaveServidor);
+			boolean retoCorrecto = decifrado.equals(reto);
 
-					flujoSalida.writeUTF(nombre);
+			//  Al recibir la respuesta, el cliente valida que el reto cifrado tenga el valor esperado; si la validaci�n pasa entonces
+			//      el cliente contin�a con el protocolo; si la validaci�n no pasa entonces el cliente termina la comunicaci�n. 
 
-					String confirmacion2 = flujoEntrada.readUTF();
+			if (retoCorrecto) {
+				System.out.println("Reto Correcto");
 
-					System.out.println(confirmacion2);
+				//TODO El cliente genera una llave sim�trica (LS),
+				//     la cifra con la llave p�blica del servidor (KS+) y la env�a al servidor. 
+				Key llaveSimetrica = ec.generateSymmetricKey();
+				String llaveEncriptada = ec.encryptKeyAsymmetric(llaveSimetrica, llaveServidor);
+				flujoSalida.writeUTF(llaveEncriptada);
+
+				// LLega la confirmaci�n ACK
+				String confirmacion = flujoEntrada.readUTF();
+
+				System.out.println(confirmacion);
+
+
+				// TODO El cliente env�a su nombre y espera un mensaje de confirmaci�n (�ACK�)
+
+
+
+				flujoSalida.writeUTF(ec.encryptMessageAsymmetric(nombre, llaveServidor));
+
+				String confirmacion2 = flujoEntrada.readUTF();
+
+				System.out.println(confirmacion2);
+				if(true) {
+
 
 					// El cliente env�a el identificador del paquete para el que est� buscando informaci�n.
 
-					System.out.println("Escriba el identificador de su paquete: ");
-
-					String paquete = scan.nextLine();
-
-					flujoSalida.writeUTF(paquete);
+					flujoSalida.writeUTF(Integer.toString(idPaquete));
 
 
 					// TODO Recibe el estado del paquete
@@ -141,21 +171,15 @@ public class Cliente {
 
 						System.out.println("Validacion Errada");
 					}
-
-				} else {
-
-					System.out.println("Termina protocolo");
+				}
+				else {
+					System.out.println("Error buscando nombre en tabla");
 				}
 
-			}else if(mensaje.equals("b")) {
+			} else {
 
-				System.out.println("Sali�");
+				System.out.println("Termina protocolo");
 			}
-
-
-
-
-
 		} catch (java.net.UnknownHostException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Unk problem");
